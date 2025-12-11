36e9579ac0670244f251bb4195699db1a888765c
RajBDevops <rajkumar.bandaru1987@gmail.com>
2025-12-11 11:19:34 +0530

    first commit

---
+++ storage.tf
@@ -0,0 +1,16 @@
+#creating storage account
+
+variable "my_storage_account" {
+    type = string
+    default = "1234tfstore1987"
+  
+}
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = var.my_storage_account
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ 
+  }
+  
